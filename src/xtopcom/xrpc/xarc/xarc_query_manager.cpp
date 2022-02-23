f9ed7826389037a9857b0b2a9799e77c3db4edca
“Guanges” <741131987@qq.com>
2022-02-23 15:39:02 +0800

    add getBlocksByHeight function

--- src/xtopcom/xrpc/xarc/xarc_query_manager.cpp
+++ src/xtopcom/xrpc/xarc/xarc_query_manager.cpp
@@ -225,7 +225,7 @@ void xarc_query_manager::getBlocksByHeight(xjson_proc_t & json_proc) {
         xJson::Value value;
         for (base::xvblock_t * vblock : vblocks) {
             data::xblock_t * bp = dynamic_cast<data::xblock_t *>(vblock);
-            value.append(get_blocks_json(bp));
+            value.append(m_bh.get_blocks_json(bp));
         }
         result_json["value"] = value;
     }

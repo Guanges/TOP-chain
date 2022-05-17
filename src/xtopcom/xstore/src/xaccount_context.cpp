61ba2fcd6201cb3a4bef2ab658b22f0eeb44b27f
“Guanges” <741131987@qq.com>
2022-05-17 20:35:42 +0800

    evm block add logsbloom and gasused

--- src/xtopcom/xstore/src/xaccount_context.cpp
+++ src/xtopcom/xstore/src/xaccount_context.cpp
@@ -126,7 +126,7 @@ int32_t xaccount_context_t::create_user_account(const std::string& address) {
     m_account->tep_token_deposit(token_name, add_token_256);
 
     token_name = data::XPROPERTY_ASSET_USDT;
-    old_token_256 = m_account->tep_token_balance(data::XPROPERTY_TEP1_BALANCE_KEY, token_name);
+    old_token_256 = m_account->tep_token_balance(token_name);
     if (old_token_256 != 0) {
         xerror("xaccount_context_t::create_user_account fail-eth token not zero");
         return -1;
@@ -134,7 +134,7 @@ int32_t xaccount_context_t::create_user_account(const std::string& address) {
     m_account->tep_token_deposit(token_name, add_token_256);
 
     token_name = data::XPROPERTY_ASSET_USDC;
-    old_token_256 = m_account->tep_token_balance(data::XPROPERTY_TEP1_BALANCE_KEY, token_name);
+    old_token_256 = m_account->tep_token_balance(token_name);
     if (old_token_256 != 0) {
         xerror("xaccount_context_t::create_user_account fail-eth token not zero");
         return -1;
